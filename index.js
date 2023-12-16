fa0f338496051bf6a7dab3a26e9535c9ecfc99ff
shahul75 <shahul75@gmail.com>
2023-12-16 17:57:45 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const moment = require('moment');
+
+const greeting = {
+    "en" : "Hello",
+    "fr" : "Bonjour",
+    "ar" : "Salam",
+    "es" : "Hola",
+    "ta" : "Vanakkam"
+ };
+
+ exports.handler = async (event) =>{
+
+    let name = event.pathParameters.name;
+    let {lang, ...info}   = event.queryStringParameters;    
+    let message = `${greeting[lang] ? greeting[lang] : greeting['en']} ${name}`;
+    let responsemessage = {
+        message: message,
+        info: info,
+        timestamp : moment().unix()
+    }
+    
+        const response = {
+        "statusCode": 200,
+         body :  JSON.stringify(responsemessage),
+        "isBase64Encoded": false
+    };
+    
+    return response;
+ };
